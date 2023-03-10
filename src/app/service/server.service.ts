0cafa3bceb48ae1405796682bee460a4f259e1e2
Gustavo <penarandagustavo5@gmail.com>
2023-03-10 17:42:02 -0500

    get server list through observable reactively

--- src/app/service/server.service.ts
+++ src/app/service/server.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { CustomResponse } from '../interface/custom-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
 
-  constructor() { }
+  private readonly apiUrl = 'any';
+
+  constructor(private http: HttpClient) { }
+
+  servers$ = <Observable<CustomResponse>>
+  this.http.get<CustomResponse>(`${this.apiUrl}/server`)
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  );
+
+  handleError(handleError: any): Observable<never> {
+    return throwError(() => { new Error('Not Implemented') });
+  }
 }
