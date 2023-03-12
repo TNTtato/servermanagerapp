619cf295051399a52d79b11dc819ff7e68c47d2c
Gustavo <penarandagustavo5@gmail.com>
2023-03-12 13:10:02 -0500

    load diferent UI depending on AppState

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ import { ServerService } from './service/server.service';
 export class AppComponent implements OnInit {
 
   appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
 
   constructor(private serverService: ServerService) {}
 
